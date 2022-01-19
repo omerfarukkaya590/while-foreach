adef628b0064506e4b4438e8f1282a267f494ea8
omerfarukkaya590 <85821993+omerfarukkaya590@users.noreply.github.com>
2022-01-19 11:46:14 +0300

    Add files via upload

---
+++ While--ForEach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace While__ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den başlayarak console den girilen sayıya kadar (sayı dahil) oratalama hesaplayıp console a yazdıran program
+            Console.Write("Lütfen bir sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac =1;
+            int toplam =0;
+            while (sayac<=sayi)
+            {
+                 toplam+= sayac;
+                 sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //A dan Z ye kadar tüm harfleri yazdır
+             char character= 'a';
+            while (character<='z')
+            {
+                 Console.Write(character);
+                 character++;
+            }
+            Console.WriteLine("*****ARABA MODELLERİ*****");
+
+            String[] arabalar ={"BMW","FORD","AUDİ","HYUNDAİ"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine("araba");
+            }
+        }
+    }
+}
